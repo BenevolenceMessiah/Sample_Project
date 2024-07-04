f5936664e34d3e728745f0122539c207acebdb3e
Benevolence Messiah <120072200+BenevolenceMessiah@users.noreply.github.com>
2024-07-04 16:06:48 -0400

    Update Push_Changes_to_Repo_Windows.bat

--- Push_Changes_to_Repo_Windows.bat
+++ Push_Changes_to_Repo_Windows.bat
@@ -1,11 +1,12 @@
 @echo on
-git add .
+git add -f .
 git status
 git commit -m "Put changes that were made here"
 git status
 git branch -M main
 git status
 git push
+git status
 git lfs push origin main --all
 git status
 
